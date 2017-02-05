3fab5a83160d184f764aaec356407084fd467859
Fabian P. Schmidt <kerel-fs@gmx.de>
2017-02-05 16:11:14 +0100

    Add simple timestamp status display

---
+++ show-timestamp.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import curses
+from curses.textpad import Textbox, rectangle
+from datetime import datetime
+
+
+def main(stdscr):
+    stdscr.addstr(0, 0, "Current time:")
+
+    editwin = curses.newwin(5,30, 2,1)
+    rectangle(stdscr, 1,0, 3, 11)
+
+    while True:
+        stdscr.addstr(2,2, "{:%H:%M:%S}".format(datetime.now()))
+        stdscr.refresh()
+
+if __name__ == "__main__":
+    stdscr = curses.initscr()
+    curses.noecho()
+    main(stdscr)
