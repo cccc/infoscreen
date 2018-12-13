2b94b83e3dd5eb89f96c499183b8921070174560
Necro <andre.kramp@necro-development.net>
2018-12-13 22:08:42 +0100

    weather window

--- infoscreen-livingroom.py
+++ infoscreen-livingroom.py
@@ -77,7 +77,7 @@ class Livingroom(Infoscreen):
                 }
             ])
                 
-        weatherw = showweather.weatherwin(68,28,36,8);
+        weatherw = showweather.weatherwin(79,28,30,8);
         self.add_window(weatherw,[
                 {
                     "subscribe" : ("weather",2),
